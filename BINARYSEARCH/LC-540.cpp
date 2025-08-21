8049ac4ecb5458aa9e31660e89441b36fd403a96
Ajay Kumar <ajaykrsingh8709@gmail.com>
2025-08-21 22:34:02 +0530

    lc-540

---
+++ BINARYSEARCH/LC-540.cpp
@@ -0,0 +1,29 @@
+using namespace std;
+#include <vector>
+
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int n = nums.size();
+        if(n==1) return nums[0];
+        if(nums[0] != nums[1]) return nums[0];
+        if(nums[n-1] != nums[n-2]) return nums[n-1];
+        int low = 1, high = n-2;
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(nums[mid] != nums[mid-1] && nums[mid] != nums[mid+1]) return nums[mid];
+            // if left half
+            if(mid%2==1 && nums[mid-1]==nums[mid] || mid%2==0 && nums[mid+1]==nums[mid]){
+                low = mid+1;  // eliminate left half
+            }
+            // if right half
+            else{
+                high = mid-1;  // eliminate right half
+            }
+        }
+        return -1;
+    }
+};
+
+
+
